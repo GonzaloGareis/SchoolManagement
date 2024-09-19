1b5f2ebe86debe9cd0ccc1e0bde5879564959280
Gonzalo Gareis <153492381+GonzaloGareis@users.noreply.github.com>
2024-09-19 13:06:07 +0200

    Update Subject.java
    Version Update

--- demo/src/main/java/com/example/demo/entity/Subject.java
+++ demo/src/main/java/com/example/demo/entity/Subject.java
@@ -1,9 +1,18 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -11,9 +20,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -23,14 +30,29 @@ import lombok.ToString;
 @Table
 public class Subject {
 
+	private String name;
+
 	@Id
 	@SequenceGenerator(name = "subject_sequence", sequenceName = "subject_sequence", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subject_sequence")
 	private long id;
 
-	private String name;
+
+	@JsonIgnore
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "enrolled_students", joinColumns = @JoinColumn(name = "subject_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+	private List<Student> enrolledStudents;
 
 	public Subject(String name) {
 		this.name = name;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "teacher_id", referencedColumnName = "id")
+	private Teacher teacher;
+
+	@Override
+	public String toString() {
+		return (this.getName() + ". Id: " + this.getId());
 	}
 }
