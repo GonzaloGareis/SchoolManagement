880233fb303bc437b26904d6582aba9f3358cb0d
Gonzalo Gareis <153492381+GonzaloGareis@users.noreply.github.com>
2024-09-19 13:07:03 +0200

    Update StudentMapper.java
    Version Update

--- demo/src/main/java/com/example/demo/mapper/StudentMapper.java
+++ demo/src/main/java/com/example/demo/mapper/StudentMapper.java
@@ -1,9 +1,23 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.example.demo.dto.StudentDTO;
+import com.example.demo.dto.SubjectDTO;
 import com.example.demo.entity.Student;
 
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
 public class StudentMapper {
+
+	@Autowired
+	private final SubjectMapper subjectMapper;
 
 	public Student DTOToEntity(StudentDTO dto) {
 		Student entity = new Student();
@@ -22,8 +36,9 @@ public class StudentMapper {
 		dto.setName(entity.getName());
 		dto.setEmail(entity.getEmail());
 		dto.setDob(entity.getDob());
-
+		List<SubjectDTO> subjectList = entity.getSubjects().stream()
+				.map(subject -> subjectMapper.entityToDTO(subject)).collect(Collectors.toList());
+		dto.setSubjects(subjectList);
 		return dto;
 	}
-
 }
