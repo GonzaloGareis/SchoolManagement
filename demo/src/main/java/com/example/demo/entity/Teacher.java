2ca4a53435a167dacf09aa222b11993e9276f5ed
Gonzalo Gareis <153492381+GonzaloGareis@users.noreply.github.com>
2024-09-19 13:06:30 +0200

    Update Teacher.java
    Version Update

--- demo/src/main/java/com/example/demo/entity/Teacher.java
+++ demo/src/main/java/com/example/demo/entity/Teacher.java
@@ -1,5 +1,45 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher {
 
+	@Id
+	@SequenceGenerator(name = "teacher_sequence", sequenceName = "teacher_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_sequence")
+	private long id;
+
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "teacher")
+	private List<Subject> subjects;
+
+
+	private String name;
+
+	public Teacher(String name) {
+		this.name = name;
+	}
 }
